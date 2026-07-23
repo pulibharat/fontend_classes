dbce3f1415a7f4b224eb6d107c34492e83fea311
puli bharat <pulibharat2007@gmail.com>
2026-07-23 16:25:09 +0530

    filter method

--- cal.js
+++ cal.js
@@ -14,3 +14,18 @@ switch (true) {
     default:
         console.log("Needs Improvement");
 }
+
+
+// student grade calculator for 6 subjects     
+const subjects = ["Math", "Science", "English", "History", "Geography", "Physical Education"];
+const subjectMarks = [85, 92, 78, 88, 95, 82];
+let totalMarks = 0;
+for (let i = 0; i < subjectMarks.length; i++) {
+    totalMarks += subjectMarks[i];
+}
+
+const averageMark = totalMarks / subjectMarks.length;
+const overallGrade = (averageMark >= 90) ? "A" : (averageMark >= 80) ? "B" : "C";
+
+console.log("Average Mark: " + averageMark);
+console.log("Overall Grade: " + overallGrade);
---
+++ filter.js
@@ -0,0 +1,4 @@
+
+const marks = [35,60,75,45,90,100];
+const filteredMarks = marks.filter((mark) => mark >= 50);
+console.log(filteredMarks);
